ef651e895c97bf2ab7ab500a3ddc6e41c8bd95c7
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-06-30 19:20:04 -0400

    pushing Book.js model

---
+++ booksearch/models/Book.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    google: {
+        type: String,
+        default: 'google_id',
+    },
+    title: {
+        type: String,
+        default: 'book_title',
+    },
+    authors: {
+        type: Array,
+        default: 'authors',
+    },
+    description: {
+        type: String,
+        default: 'description',
+    },
+    images: {
+        type: String,
+        default: 'https://dummyimage.com/287.98X479.22/222222/fff&text=photo+unavailable',
+    },
+    link: {
+        type: String,
+        default: 'more_info',
+    },
+});
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
