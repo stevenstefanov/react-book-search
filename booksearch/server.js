f55d59e1dd428ae73c3ab7b9f60eb5063ed6520f
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-06-30 18:57:11 -0400

    initial commit with server.js.

---
+++ booksearch/server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const exporess = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+if (process.end.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Now listening on PORT ${PORT}!`);
+});
