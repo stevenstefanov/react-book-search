c4a80485385d5caec900d3f15c725a891c91cd51
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-07-02 12:26:08 -0400

    pushing googleBooks.js

---
+++ booksearch/client/src/pages/GoogleBooks/GoogleBooks.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Header from '../../components/Header/Header';
+import Search from '../../components/Search/Search';
+import useDebounce from '../../utils/debounceHook';
+import API from '../../utils/API';
+
+import './googleBooks.css';
+
+const GoogleBooks = () => {
+    const [results, setResults] = useState([]);
+    const [input, setInput] = useState('');
+    const debouncedSearchTerm = useDebounce(input, 500);
+
+    useEffect(() => {
+        if (!input) {
+            return;
+        }
+        if (debouncedSearchTerm) {
+            API.GoogleBooks(input).then(({ data: { item } }) => {
+                setResults(item);
+            });
+        }
+    }, [debouncedSearchTerm]);
+
+    return (
+        <div className='wrapper'>
+            <Container className='main-body m-5'>
+                <div className='row'>
+                    <div className='col'>
+                        <Header title='Google Books Search' />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <Search
+                            input={input}
+                            setInput={setInput}
+                            setResults={setResults}
+                            results={results}
+                        />
+                    </div>
+                </div>
+                {results !== [] && <Results results={results} />}      
+            </Container>
+        </div>
+    );
+};
+
+export default GoogleBooks;
